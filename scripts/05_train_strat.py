da7575d0674cd683329f2be845671b74dff04f50
donghyun Kim <dk58319@ajou.ac.kr>
2023-06-07 07:15:57 +0000

    add auroc

--- scripts/05_train_strat.py
+++ scripts/05_train_strat.py
@@ -1,11 +1,11 @@
 import os
 
 import numpy as np
-from torchmetrics.classification import MultilabelAUROC
+from torcheval.metrics import BinaryAUROC
 from torch.utils.data import DataLoader, random_split
 import torch
 from tqdm import tqdm
-from dataset import ECG_dataset
+from dataset_strat import ECG_dataset
 import torchvision.transforms as transforms
 from torch import optim
 from torch import nn
@@ -13,6 +13,10 @@ import torchvision
 import sys
 import timm
 import wandb
+from torch.utils.data.sampler import SubsetRandomSampler
+
+from sklearn.model_selection import train_test_split,StratifiedShuffleSplit
+
 
 wandb.init(project='medical_ecg')
 wandb.config = {
@@ -27,7 +31,7 @@ args = {
         "LEARNING_RATE" : 0.001,
         "WEIGHT_DECAY" : 0.003,
         "BATCH_SIZE" : 64,
-        "NUM_EPOCHS" : 150,
+        "NUM_EPOCHS" : 100,
         "MEAN" : (0.485, 0.456, 0.406),
         "STD" : (0.229, 0.224, 0.225),
         "BETA" : 0,
@@ -42,56 +46,69 @@ transform = transforms.Compose(
         transforms.ToTensor(),
     ]
 )
+test_transform = transforms.Compose(
+    [
+        transforms.ToTensor(),
+    ]
+)
 
 dataset = ECG_dataset(csv_path, img_path, transform=transform)
 dataset_len = len(dataset)
 print(dataset_len)
-train_size = int(17440)
-val_size = int(dataset_len - 17440)
 
-train_dataset, validation_dataset = random_split(
-    dataset, [train_size, val_size])
+train_dataset = ECG_dataset(csv_path, img_path, transform=transform)
+test_dataset = ECG_dataset(csv_path, img_path, transform= test_transform)
 
-train_loader = DataLoader(train_dataset, batch_size=64, shuffle=True, num_workers=8)
-valid_loader = DataLoader(
-    validation_dataset, batch_size=64, shuffle=True, num_workers=8)
+train_indices = list(train_dataset.train_df.index)
+test_indices = list(test_dataset.test_df.index)
+
+train_sampler = SubsetRandomSampler(train_indices)
+train_loader = DataLoader(train_dataset, batch_size=args["BATCH_SIZE"], sampler=train_sampler, num_workers= 8)
+
+test_sampler = SubsetRandomSampler(test_indices)
+test_loader = DataLoader(test_dataset, batch_size=args["BATCH_SIZE"], sampler=test_sampler, num_workers= 8)
+
+
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 model = timm.create_model(args["MODEL"], pretrained=True, num_classes=2).to(device)
 
 
-optimizer = optim.AdamW(model.parameters(), lr=0.00001)
+optimizer = optim.AdamW(model.parameters(), lr=0.0001)
 criterion = nn.CrossEntropyLoss()
-scheduler = optim.lr_scheduler.OneCycleLR(optimizer, max_lr=0.001,total_steps=args["NUM_EPOCHS"],steps_per_epoch=len(train_loader), epochs=args["NUM_EPOCHS"])
+scheduler = optim.lr_scheduler.OneCycleLR(optimizer, max_lr=0.008,total_steps=args["NUM_EPOCHS"],steps_per_epoch=len(train_loader), epochs=args["NUM_EPOCHS"])
+metric = BinaryAUROC()
+
 
 train_losses, validation_losses = [], []
 
+def validation(model, test_loader, criterion):
 
-
-def validation(model, valid_loader, criterion):
-    auroc = MultilabelAUROC(num_labels=2, average="macro", thresholds=None)
     accuracy = 0
     valid_loss = 0
-
-    for i, (X, y) in enumerate(valid_loader):
+    auroc = 0
+    
+    
+    for i, (X, y) in enumerate(test_loader):
         if torch.cuda.is_available():
             X = X.to(args["DEVICE"])
             y = y.type(torch.LongTensor)
             y = y.to(args["DEVICE"])
-
+        
         outputs = model(X)
         loss = criterion(outputs, y)
-        out_auroc= auroc(outputs,y)
+        #metric.update(X, y)
+        #auroc = metric.compute()
         valid_loss += loss.item()
         outputs_ = torch.argmax(outputs, dim=1)
         
         accuracy += (outputs_ == y).float().sum()
     
 
-    return valid_loss, accuracy, auroc
+    return valid_loss, accuracy
 
 
-def train_model(model, train_loader, valid_loader, criterion, optimizer, args, fold_num=1):
+def train_model(model, train_loader, test_loader, criterion, optimizer, args, fold_num=1):
     steps = 0
     total_step = len(train_loader)
     best_val = np.inf
@@ -122,21 +139,21 @@ def train_model(model, train_loader, valid_loader, criterion, optimizer, args, f
             if steps % total_step == 0:
                 model.eval()
                 with torch.no_grad():
-                    valid_loss, accuracy, auroc = validation(model, valid_loader, criterion)
+                    valid_loss, accuracy= validation(model, test_loader, criterion)
 
                 print("Epoch: {}/{}.. ".format(epoch + 1, args['NUM_EPOCHS']) +
                       "Training Loss: {:.5f}.. ".format(running_loss / total_step) +
-                      "Valid Loss: {:.5f}.. ".format(valid_loss / len(valid_loader)) +
-                      "Valid Accuracy: {:.5f}.. ".format(accuracy / len(valid_loader.dataset)) )
-                wandb.log({"loss": (running_loss / total_step),"Valid loss":valid_loss / len(valid_loader) ,"Valid Accuracy": (accuracy / len(valid_loader.dataset)), "AUROC":auroc})
+                      "Valid Loss: {:.5f}.. ".format(valid_loss / len(test_loader)) +
+                      "Valid Accuracy: {:.5f}.. ".format(accuracy / len(test_loader.dataset)) )
+                wandb.log({"loss": (running_loss / total_step),"Valid loss":valid_loss / len(test_loader) ,"Valid Accuracy": (accuracy / len(test_loader.dataset))})
                 # Save Model
-                if (valid_loss / len(valid_loader)) < best_val:
-                    best_val = (valid_loss / len(valid_loader))
+                if (valid_loss / len(test_loader)) < best_val:
+                    best_val = (valid_loss / len(test_loader))
                     torch.save(model.state_dict(), f"{args['MODEL_PATH']}/"+f"{args['MODEL']}.pt")
-                    print("------ model saved ------- : {:.5f}".format((accuracy/len(valid_loader.dataset))*100))
+                    print("------ model saved ------- : {:.5f}".format((accuracy/len(test_loader.dataset))*100))
                 
                 train_losses.append(running_loss / len(train_loader))
-                validation_losses.append(valid_loss / len(valid_loader))
+                validation_losses.append(valid_loss / len(test_loader))
                 steps = 0
                 running_loss = 0
                 model.train()
@@ -145,5 +162,5 @@ def train_model(model, train_loader, valid_loader, criterion, optimizer, args, f
 
     return 
 
-train_model(model, train_loader, valid_loader, criterion, optimizer, args, fold_num=1)
+train_model(model, train_loader, test_loader, criterion, optimizer, args, fold_num=1)
 
--- scripts/dataset_strat.py
+++ scripts/dataset_strat.py
@@ -8,10 +8,12 @@ import torchvision.transforms as transforms
 import torchvision.transforms.functional as TF
 import torchvision
 import torch.nn.functional as F
-
+from sklearn.model_selection import StratifiedShuffleSplit
 
 path = '/home/dk58319/private/workbench/results/pngfiles'
 csv_path = '/home/dk58319/private/workbench/results/output_files/MI_annotation.csv'
+
+
 
 
 class ECG_dataset(Dataset):
@@ -22,21 +24,26 @@ class ECG_dataset(Dataset):
         self.df = pd.read_csv(csv_path)
         self.df.fillna(0)
         self.image_names = self.df['ecg_id']
-        # self.labels = self.df.iloc[:, 1:10].values
         self.labels = self.df['non']
+
+        # Stratified Shuffle Split 수행하여 train 및 test 인덱스 얻기
+        sss = StratifiedShuffleSplit(n_splits=1, test_size=0.2, random_state=42)
+        train_idx, test_idx = next(sss.split(self.df, self.labels))
+
+        # train 및 test 데이터셋 생성
+        self.train_df = self.df.iloc[train_idx]
+        self.test_df = self.df.iloc[test_idx]
 
     def __len__(self):
         return len(self.df)
 
     def __getitem__(self, index):
-        image = Image.open(os.path.join(self.path, str(
-            self.image_names[index]).zfill(5)+'.png'))
+        image = Image.open(os.path.join(self.path, str(self.image_names[index]).zfill(5)+'.png'))
         image = image.convert('RGB')
         image = image.resize((384, 384))
         if self.transform is not None:
             image = self.transform(image)
-        label = self.df.iloc[index]['non']
-        # label = self.labels
-        # label = F.one_hot(label.to(torch.int64), num_classes=9)
-
+        
+        label = self.labels[index]
+        
         return image, label
